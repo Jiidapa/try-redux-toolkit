154f511f7b217ac413ba1deca13388a90640a1c5
jidapa <jidapa.p@kkumail.com>
2021-04-02 00:45:06 +0700

    [+] add counter function

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,59 +1,19 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { inclement, decrement, selectCount } from "./CounterSlice";
 
-export function Counter() {
-  const count = useSelector(selectCount);
+export default function Counter() {
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
-
+  const count = useSelector(selectCount);
   return (
     <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
+      My Counter
+      <div>
+        <button onClick={() => dispatch(inclement())}>Increment counter</button>
       </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() =>
-            dispatch(incrementByAmount(Number(incrementAmount) || 0))
-          }
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
-          Add Async
-        </button>
+      <div>{count}</div>
+      <div>
+        <button onClick={() => dispatch(decrement())}>Decrement counter</button>
       </div>
     </div>
   );
